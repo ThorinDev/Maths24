fbf481f5b26525cd83de6bb520712e43bad5a250
Chris Nethercott <c.nethercott@icloud.com>
2016-11-13 16:22:38 +0000

    Change fur to fou

--- src/com/thorindev/Solver24.java
+++ src/com/thorindev/Solver24.java
@@ -4,6 +4,7 @@ public class Solver24 {
 
     private int[] num;
 
+    public Solver24(int one, int two, int thr, int fou) {
         this.num[0] = one;
         this.num[1] = two;
         this.num[2] = thr;
