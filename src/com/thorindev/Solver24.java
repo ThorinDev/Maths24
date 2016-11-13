26e99dff5cb383d9a2c9d78396b49f9f1618682d
Chris Nethercott <c.nethercott@icloud.com>
2016-11-13 16:23:52 +0000

    renamed the in ones, it is just easier

--- src/com/thorindev/Solver24.java
+++ src/com/thorindev/Solver24.java
@@ -4,12 +4,11 @@ public class Solver24 {
 
     private int[] num;
 
-    public Solver24(int one, int two, int thr, int fou) {
-        this.num[0] = one;
-        this.num[1] = two;
-        this.num[2] = thr;
-        this.num[3] = fou;
+    public Solver24(int a, int b, int c, int d) {
+        this.num[0] = a;
+        this.num[1] = b;
+        this.num[2] = c;
+        this.num[3] = d;
     }
-
 
 }
