e019c9ea8b3e038703ff2dae7f5e436de6664f85
Chris Nethercott <c.nethercott@icloud.com>
2016-11-13 16:18:14 +0000

    Added Maths24.java

---
+++ src/com/thorindev/Maths24.java
@@ -0,0 +1,9 @@
+package com.thorindev;
+
+public class Maths24 {
+
+    public static void main(String[] args) {
+        System.out.println("Maths24 is booting up!");
+	// write your code here
+    }
+}
