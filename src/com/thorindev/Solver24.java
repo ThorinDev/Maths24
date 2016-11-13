94dd8f887a3462c913097d8b55d6ec0e6347d2c1
Chris Nethercott <c.nethercott@icloud.com>
2016-11-13 16:18:19 +0000

    Created the solver

---
+++ src/com/thorindev/Solver24.java
@@ -0,0 +1,18 @@
+package com.thorindev;
+
+public class Solver24 {
+
+    private int one;
+    private int two;
+    private int thr;
+    private int fur;
+
+    public Solver24(int one, int two, int thr, int fur) {
+        this.one = one;
+        this.two = two;
+        this.thr = thr;
+        this.fur = fur;
+    }
+
+
+}
