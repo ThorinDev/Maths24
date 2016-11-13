4b8d5fba64c31c3aee51da19900c4dc9ea2d22c7
Chris Nethercott <c.nethercott@icloud.com>
2016-11-13 16:42:16 +0000

    stuff

--- src/com/thorindev/Solver24.java
+++ src/com/thorindev/Solver24.java
@@ -2,13 +2,23 @@ package com.thorindev;
 
 public class Solver24 {
 
-    private int[] num;
+    public int[] num;
+    private char pl = '+';
+    private char mi = '-';
+    private char ti = '*';
+    private char di = '/';
 
     public Solver24(int a, int b, int c, int d) {
         this.num[0] = a;
         this.num[1] = b;
         this.num[2] = c;
         this.num[3] = d;
+        solve()
+    }
+
+    public String solve(int[] numbersToSolve) {
+
+        return "";
     }
 
 }
